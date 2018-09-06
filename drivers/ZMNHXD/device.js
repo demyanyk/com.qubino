9302262daf9a367aefc652bb24c8adcf2ce1c45c
Robin Bolscher <robinbolscher@gmail.com>
2018-09-06 11:26:29 +0200

    ZMNHXD WIP

--- drivers/ZMNHXD/device.js
+++ drivers/ZMNHXD/device.js
@@ -7,6 +7,7 @@ const QubinoDevice = require('../../lib/QubinoDevice');
  * 3-Phase Smart Meter (ZMNHXD)
  * Extended manual: http://qubino.com/download/2244/
  * TODO: test endpoints and add relay endpoints?
+ * TODO: wait for response by Qubino (device seems to have faulty firmware, no METER command class on multi channel nodes)
  */
 class ZMNHXD extends QubinoDevice {
 
@@ -22,16 +23,22 @@ class ZMNHXD extends QubinoDevice {
 	 * @private
 	 */
 	registerCapabilities() {
-		this.registerCapability(constants.capabilities.onoff, constants.commandClasses.switchBinary);
-		this.registerCapability(constants.capabilities.measureVoltage, constants.commandClasses.meter);
-		this.registerCapability(constants.capabilities.measureCurrent, constants.commandClasses.meter);
-		this.registerCapability(constants.capabilities.measurePower, constants.commandClasses.meter);
-		this.registerCapability(constants.capabilities.meterPowerImport, constants.commandClasses.meter);
+		this.node.CommandClass.COMMAND_CLASS_METER.on('report', report => {
+			console.log('report', report);
+		});
+		this.log('isMultiChannelNode', this.node.isMultiChannelNode, this.node.multiChannelNodeId);
+		const mcObj = {};
+		if (!this.node.isMultiChannelNode) mcObj.multiChannelNodeId = 1;
+		this.registerCapability(constants.capabilities.onoff, constants.commandClasses.switchBinary, mcObj);
+		this.registerCapability(constants.capabilities.measureVoltage, constants.commandClasses.meter, mcObj);
+		this.registerCapability(constants.capabilities.measureCurrent, constants.commandClasses.meter, mcObj);
+		this.registerCapability(constants.capabilities.measurePower, constants.commandClasses.meter, mcObj);
+		this.registerCapability(constants.capabilities.meterPowerImport, constants.commandClasses.meter, mcObj);
 		this.registerCapability(constants.capabilities.meterPowerExport, constants.commandClasses.meter);
-		this.registerCapability(constants.capabilities.powerReactive, constants.commandClasses.meter); // TODO: validate this is in kVar
-		this.registerCapability(constants.capabilities.powerTotalReactive, constants.commandClasses.meter);
-		this.registerCapability(constants.capabilities.powerTotalApparent, constants.commandClasses.meter);
-		this.registerCapability(constants.capabilities.powerFactor, constants.commandClasses.meter);
+		this.registerCapability(constants.capabilities.powerReactive, constants.commandClasses.meter, mcObj); // TODO: validate this is in kVar
+		this.registerCapability(constants.capabilities.powerTotalReactive, constants.commandClasses.meter, mcObj);
+		this.registerCapability(constants.capabilities.powerTotalApparent, constants.commandClasses.meter, mcObj);
+		this.registerCapability(constants.capabilities.powerFactor, constants.commandClasses.meter, mcObj);
 	}
 }
 
